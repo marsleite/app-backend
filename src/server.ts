1c9f5cec3eac558f46313a7a6b9640e5a90bd8aa
Marcelo <marsleite@gmail.com>
2021-12-15 21:58:15 -0300

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 const PORT = 3333;
+
+app.get("/", (_req: Request, res: Response) => {
+  return res.json({ message: 'Hello, world' })
+});
 
 app.listen(PORT, () => {
   console.log(`Rodando na porta ${PORT}`);
